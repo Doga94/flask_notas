2d2b3739f8ff588fb60f4176039cf18934578208
unknown <doga19941@hotmail.com>
2025-07-20 10:06:01 -0500

    Registro, Login y Control de Sesión en Flask

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect, url_for, flash
+from flask import Blueprint, render_template, request, redirect, url_for, flash, session
 from app.models import Note, User
 from app import db
 
@@ -6,11 +6,19 @@ main = Blueprint('main', __name__)
 
 @main.route("/")
 def home():
-    notes = Note.query.all()
+    # notes = Note.query.all()
+    if "user_id" not in session:
+        flash("Debe iniciar sesión")
+        return redirect(url_for("main.login"))
+    notes = Note.query.filter_by(user_id=session["user_id"]).all()
     return render_template("home.html", notes=notes)
 
 @main.route("/create", methods=["GET", "POST"])
 def create_note():
+    if "user_id" not in session:
+        flash("Debe iniciar sesión para crear notas")
+        return redirect(url_for('main.login'))
+
     if request.method == "POST":
         title = request.form.get("title", "").strip()
         content = request.form.get("content", "").strip()
@@ -19,9 +27,9 @@ def create_note():
             flash("❌ Todos los campos son obligatorios.")
             return render_template("note_form.html")
 
-        user = User.query.filter_by(username="David").first()
+        user_id = session["user_id"]
+        note = Note(title=title, content=content, user_id=user_id)
 
-        note = Note(title=title, content=content, user=user)
         db.session.add(note)
         db.session.commit()
 
@@ -58,9 +66,54 @@ def delete_note(note_id):
     flash("🗑️ Nota eliminada con éxito")
     return redirect(url_for("main.home"))
 
-@main.before_app_request
-def create_sample_user():
-    if not User.query.filter_by(username="David").first():
-        user = User(username="David", email="david@example.com")
+@main.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method== "POST":
+        username = request.form.get("username", "").strip()
+        email = request.form.get("email", "").strip()
+
+        if not username or not email:
+            flash("❌ Todos los campos son obligatorios")
+            return render_template("register.html")
+        
+        if User.query.filter_by(username=username).first():
+            flash("❌ El usuario ya eiste")
+            return render_template("register.html")
+        
+        user = User(username=username, email=email)
         db.session.add(user)
         db.session.commit()
+
+        flash("✅ Registro exitoso, se puede iniciar sesión")
+        return redirect(url_for("main.login"))
+    return render_template("register.html")
+
+@main.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method=="POST":
+        username = request.form.get("username", "").strip()
+
+        user = User.query.filter_by(username=username).first()
+        if not user:
+            flash("❌ Usuario no encontrado")
+            return render_template("login.html")
+        
+        session["user_id"] = user.id
+        session["username"] = user.username
+        flash(f"✅ Bienvenido, {user.username}")
+        return redirect(url_for("main.home"))
+    return render_template("login.html")
+
+@main.route("/logout")
+def logout():
+    session.clear()
+    flash("Has cerrado sesión")
+    return redirect(url_for("main.login"))
+
+#Esto es solo para validar los usuarios es una test
+# @main.before_app_request
+# def create_sample_user():
+#     if not User.query.filter_by(username="David").first():
+#         user = User(username="David", email="david@example.com")
+#         db.session.add(user)
+#         db.session.commit()
--- run.py
+++ run.py
@@ -3,9 +3,9 @@ from app.models import User, Note
 
 app = create_app()
 
-with app.app_context():
-    db.drop_all()
-    db.create_all()
+# with app.app_context():
+#     db.drop_all()
+#     db.create_all()
 
 if __name__ == "__main__":
     app.run(debug=True)
